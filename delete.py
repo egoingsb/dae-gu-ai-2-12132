f93a9ca94112684ee39fa691ff47127666f31497
egoing <egoing@gmail.com>
2021-12-13 22:08:43 +0900

    삭제

---
+++ delete.py
@@ -0,0 +1,7 @@
+import sqlite3
+with sqlite3.connect('data.db') as connect:
+    cursor = connect.cursor()
+
+    id = input('아이디를 입력해주세요: ')
+    sql = 'DELETE FROM topics WHERE id = ?'
+    cursor.execute(sql, (id,))
