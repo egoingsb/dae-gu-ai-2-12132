af84ab6a39f9794a30e947fe23d5ab7b8ee5ff71
egoing <egoing@gmail.com>
2021-12-13 21:38:22 +0900

    마지막 행의 식별자를 알아내는 법

--- create.py
+++ create.py
@@ -1,13 +1,12 @@
 import sqlite3
-connect = sqlite3.connect('data.db')
-cursor = connect.cursor()
+# connect = sqlite3.connect('data.db')
+with sqlite3.connect('data.db') as connect:
+    cursor = connect.cursor()
 
-title = input('제목을 알려주세요: ')
-body = input('본문을 알려주세요: ')
+    title = input('제목을 알려주세요: ')
+    body = input('본문을 알려주세요: ')
 
-sql = 'INSERT INTO topics (title, body) VALUES(?,?)'
-cursor.execute(sql, (title, body))
+    sql = 'INSERT INTO topics (title, body) VALUES(?,?)'
+    cursor.execute(sql, (title, body))
 
-connect.commit()
-cursor.close()
-connect.close()
+    print('id', cursor.lastrowid)
