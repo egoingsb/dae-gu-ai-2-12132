63fcbaa8239e90051c77818497b482aa8b9982c8
egoing <egoing@gmail.com>
2021-12-14 15:27:24 +0900

    delete

--- delete.py
+++ delete.py
@@ -4,4 +4,4 @@ with sqlite3.connect('data.db') as connect:
 
     id = input('아이디를 입력해주세요: ')
     sql = 'DELETE FROM topics WHERE id = ?'
-    cursor.execute(sql, (id,))
+    cursor.execute(sql, [id])
